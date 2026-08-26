da4ca47345f75a4a7a72acac4a519d4f961d1db1
soliman <soft9.me@gmail.com>
2026-08-26 16:05:32 +0300

    first commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import "@/global.css";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <SQLiteProvider
+      databaseName="qoran.db"
+      assetSource={{
+        assetId: require("../../assets/database/quran.db"),
+      }}
+    >
+      <Stack screenOptions={{ headerShown: false }} />
+    </SQLiteProvider>
   );
 }
